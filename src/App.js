90cc421b0d30bb6306c780744bac01af751d4e0b
Thomas NICOLAS <tom.nicolas@protonmail.com>
2022-12-28 16:48:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+		<Routes>
+			<Route path="/" element={<Home/>}></Route>
+		</Routes>      
+    </div>
+  )
+}
+
+export default App
