f833f4a17dda07f37d53e0eee89638aba61242a1
Thomas NICOLAS <tom.nicolas@protonmail.com>
2023-01-12 09:31:11 +0100

    comment Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,16 @@
+// Import packages and components 
 import React from "react"
 import Banner from '../banner'
 import styled from 'styled-components'
 import Gallery from "../gallery"
 
+// Stylize the elements of the page
 const StyledContainer = styled.div`
     min-height: calc(100vh - 358px);
     padding: 0 20px;
 `
 
+// Export the main function of the page
 export default function Home() {
     return (
     <StyledContainer>
